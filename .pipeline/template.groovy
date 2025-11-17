5ec080411e5613190a8dbd36a8b78cb6a4380382
Saúl Espinosa Martínez <saulem1997@gmail.com>
2025-11-17 17:27:46 -0600

    Fix template!

--- .pipeline/template.groovy
+++ .pipeline/template.groovy
@@ -1,15 +1,17 @@
-// Load stage scripts
-def buildStage = load "${env.WORKSPACE}/.pipeline/stages/build.groovy"
-def testStage = load "${env.WORKSPACE}/.pipeline/stages/test.groovy"
-def deployStage = load "${env.WORKSPACE}/.pipeline/stages/deploy.groovy"
-
 node {
+    // Load stage scripts inside node
+    def buildStage = load "${env.WORKSPACE}/.pipeline/stages/build.groovy"
+    def testStage = load "${env.WORKSPACE}/.pipeline/stages/test.groovy"
+    def deployStage = load "${env.WORKSPACE}/.pipeline/stages/deploy.groovy"
+
     stage('Build') {
         buildStage.buildStage()
     }
+
     stage('Test') {
         testStage.testStage()
     }
+
     stage('Deploy') {
         deployStage.deployStage()
     }
