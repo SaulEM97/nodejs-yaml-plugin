8ba524be01ad437172a7249cbb261ea4b4494b4e
Saúl Espinosa Martínez <saulem1997@gmail.com>
2025-11-17 17:30:58 -0600

    Fix

--- .pipeline/template.groovy
+++ .pipeline/template.groovy
@@ -1,19 +1,14 @@
 node {
-    // Load stage scripts inside node
+    stage('Checkout') {
+        checkout scm  // JTE usually handles this automatically
+    }
+
     def buildStage = load "${env.WORKSPACE}/.pipeline/stages/build.groovy"
     def testStage = load "${env.WORKSPACE}/.pipeline/stages/test.groovy"
     def deployStage = load "${env.WORKSPACE}/.pipeline/stages/deploy.groovy"
 
-    stage('Build') {
-        buildStage.buildStage()
-    }
-
-    stage('Test') {
-        testStage.testStage()
-    }
-
-    stage('Deploy') {
-        deployStage.deployStage()
-    }
+    stage('Build') { buildStage.buildStage() }
+    stage('Test') { testStage.testStage() }
+    stage('Deploy') { deployStage.deployStage() }
 }
 
