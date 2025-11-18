e52f9a451dd12b135976c2187497cf21830769bc
Saúl Espinosa Martínez <saulem1997@gmail.com>
2025-11-18 10:42:39 -0600

    Fix template and build

--- .pipeline/stages/build.groovy
+++ .pipeline/stages/build.groovy
@@ -1,8 +1,12 @@
 def buildStage() {
     sh """
-        rm -rf node_modules
+        echo "Cleaning old dependencies..."
+        rm -rf node_modules package-lock.json
+
+        echo "Running npm install..."
         npm install
     """
 }
+
 return this
 
--- .pipeline/template.groovy
+++ .pipeline/template.groovy
@@ -1,30 +1,37 @@
+// JTE Pipeline Template
+
 node {
     stage('Checkout') {
+        // Fully wipe the workspace BEFORE checkout
         deleteDir()
-        sh 'git clone https://github.com/SaulEM97/nodejs-yaml-plugin.git .'
+
+        // Use native Jenkins Git checkout
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name: '*/main']],
+            userRemoteConfigs: [[
+                url: 'https://github.com/SaulEM97/nodejs-yaml-plugin.git'
+            ]]
+        ])
     }
 
     stage('Load stages') {
+        // Always declare scripts with def
         def buildStage  = load ".pipeline/stages/build.groovy"
         def testStage   = load ".pipeline/stages/test.groovy"
         def deployStage = load ".pipeline/stages/deploy.groovy"
 
-        // expose them to later stages
-        this.buildStage  = buildStage
-        this.testStage   = testStage
-        this.deployStage = deployStage
-    }
+        stage('Build') {
+            buildStage.buildStage()
+        }
 
-    stage('Build') {
-        buildStage.buildStage()
-    }
+        stage('Test') {
+            testStage.testStage()
+        }
 
-    stage('Test') {
-        testStage.testStage()
-    }
-
-    stage('Deploy') {
-        deployStage.deployStage()
+        stage('Deploy') {
+            deployStage.deployStage()
+        }
     }
 }
 
