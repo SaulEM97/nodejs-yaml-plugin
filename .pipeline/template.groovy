ee7aeb9f9c6f72d008ed2b7331798e8866e1be97
Saúl Espinosa Martínez <saulem1997@gmail.com>
2025-11-18 10:20:17 -0600

    Fix template for npm install issue

--- .pipeline/template.groovy
+++ .pipeline/template.groovy
@@ -1,20 +1,25 @@
 node {
     stage('Checkout') {
-        // Remove workspace if needed
-        deleteDir()  // Optional, ensures clean workspace
-
-        // Clone the repo
+        deleteDir()
         sh 'git clone https://github.com/SaulEM97/nodejs-yaml-plugin.git .'
     }
 
     stage('Load stages') {
-        def buildStage = load ".pipeline/stages/build.groovy"
-        def testStage = load ".pipeline/stages/test.groovy"
-        def deployStage = load ".pipeline/stages/deploy.groovy"
+        buildStage  = load ".pipeline/stages/build.groovy"
+        testStage   = load ".pipeline/stages/test.groovy"
+        deployStage = load ".pipeline/stages/deploy.groovy"
+    }
 
-        stage('Build') { buildStage.buildStage() }
-        stage('Test') { testStage.testStage() }
-        stage('Deploy') { deployStage.deployStage() }
+    stage('Build') {
+        buildStage.buildStage()
+    }
+
+    stage('Test') {
+        testStage.testStage()
+    }
+
+    stage('Deploy') {
+        deployStage.deployStage()
     }
 }
 
